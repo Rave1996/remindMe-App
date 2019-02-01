a5ce6b0da909fe35d60bc81897c1dd9a2c0dca3b
Rave1996 <michaeldvdc@hotmail.com>
2019-01-31 22:15:39 -0600

    feature/ButtonModal

--- src/screens/Tasks/TasksStyles.js
+++ src/screens/Tasks/TasksStyles.js
@@ -1,16 +1,16 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
+    header: {
         alignItems: 'center',
-        width: '100%',
         height: 250,
+        justifyContent: 'center',
+        width: '100%',
     },
     avatar: {
+        borderRadius: 50,
         height: 100,
         width: 100,
-        borderRadius: 50,
     },
     toDo: {
         color: 'white',
@@ -19,6 +19,20 @@ const styles = StyleSheet.create({
     date: {
         color: '#C0C0C0',
         fontSize: 16,
+    },
+    button: {
+        alignSelf: 'flex-end',
+        bottom: 10,
+        position: 'absolute',
+        right: 10,
+    },
+    btnAdd: {
+        alignItems: 'center',
+        backgroundColor: 'rgba(236, 23, 73, .8)', // original: #ED184A
+        borderRadius: 50,
+        height: 75,
+        justifyContent: 'center',
+        width: 75,
     },
 });
 
--- src/screens/Tasks/index.js
+++ src/screens/Tasks/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Image, ImageBackground} from 'react-native';
+import {Modal, View, Text, Image, ImageBackground, TouchableOpacity} from 'react-native';
 import styles from './TasksStyles';
 
 const days = [ "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "SÁbado" ];
@@ -8,14 +8,21 @@ const months = [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 export default class Tasks extends Component {
     render(){
         return(
-            <View>
-                <ImageBackground source={ require('./../../img/tasks-bg.jpg') } style={styles.container} blurRadius={15}>
-                    <Image source={ require('./../../img/user-avatar.png') } style={styles.avatar}></Image>
-                    <View>
-                        <Text style={styles.toDo}>6 Pendientes</Text>
-                        <Text style={styles.date}>Fecha actual</Text>
-                    </View>
-                </ImageBackground>
+            <View style={{flex:1}}>
+                <View>
+                    <ImageBackground source={ require('./../../img/tasks-bg.jpg') } style={styles.header} blurRadius={15}>
+                        <Image source={ require('./../../img/user-avatar.png') } style={styles.avatar}></Image>
+                        <View>
+                            <Text style={styles.toDo}>6 Pendientes</Text>
+                            <Text style={styles.date}>Fecha actual</Text>
+                        </View>
+                    </ImageBackground>
+                </View>
+                <View style={styles.button}>
+                    <TouchableOpacity style={styles.btnAdd}>
+                        <Image source={ require('./../../img/icon-plus.png') }></Image>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
